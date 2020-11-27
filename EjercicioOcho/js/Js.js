87c4ec51668e2904fb2d8999b791c7bbbdde24e2
Henry <hsercas@gmail.com>
2020-11-26 23:11:01 -0500

    Actualizacion y ejercicio 8

---
+++ EjercicioOcho/js/Js.js
@@ -0,0 +1,103 @@
+
+
+let codigo = 0;
+let contrasena=0;
+function calcular() {
+    codigo = Number(prompt('Ingrese el codigo de usuario'));   
+    contrasena = Number(prompt('Ingrese la contraseña de usuario')); 
+    sumar(codigo,contrasena);
+}
+
+
+function calcular1() {
+    codigo = Number(prompt('Ingrese el codigo de usuario'));   
+    contrasena = Number(prompt('Ingrese la contraseña de usuario')); 
+    sumar2(codigo,contrasena);
+}
+
+function calcular2() {
+
+    codigo = Number(prompt('Ingrese el codigo de usuario'));   
+    contrasena = Number(prompt('Ingrese la contraseña de usuario')); 
+    sumar2(codigo,contrasena);
+
+    
+    
+}
+
+
+function sumar(p1,p2) {
+
+    let contador = 0;
+      
+
+    while (contador <= 2) {
+
+        if(p1 == 1 && p2 == 1234 ){
+
+            alert(' !Usuario identificado!');
+            document.getElementById('resultado').innerHTML='Bienvenido usuario # 1 '
+            break;
+
+        }else{
+
+            p1 = Number(prompt('Ingrese el codigo de usuario intento numero: '+(contador+1)+' # max. intentos 3'));   
+            p2 = Number(prompt('Ingrese la contraseña de usuario'));  
+            contador++
+        } 
+
+        
+           
+    }
+
+    return 0;
+}
+
+
+function sumar1(p1,p2) {
+
+    let contador = 0;
+        
+
+    do {        
+        if(p1 == 1 && p2 == 1234 ){
+
+            alert(' !Usuario identificado!');
+            document.getElementById('resultado').innerHTML='Bienvenido usuario # 1 '
+            break;
+
+        }else{
+
+            p1 = Number(prompt('Ingrese el codigo de usuario intento numero: '+(contador+1)+' # max. intentos 3'));   
+            p2 = Number(prompt('Ingrese la contraseña de usuario'));  
+            contador++
+        } 
+        
+    
+    } while (contador <= 3)
+
+    return 0;
+}
+
+
+function sumar2(codigo,contrasena) {
+
+    for(let i = 0; i < 3; i++){
+        
+        if(codigo == 1 && contrasena == 1234 ){
+
+            alert(' !Usuario identificado!');
+            document.getElementById('resultado').innerHTML='Bienvenido usuario # 1 '
+            break;
+
+        }else{
+
+            codigo = Number(prompt('Ingrese el codigo de usuario intento numero: '+(i+1)+' # max. intentos 3'));   
+            contrasena = Number(prompt('Ingrese la contraseña de usuario'));  
+        } 
+    } 
+
+   
+
+    return 0;
+}
--- EjercicioSiete/js/Js.js
+++ EjercicioSiete/js/Js.js
@@ -1,6 +1,11 @@
 
-'use strict';
+'use strict'
+
 let numero = 3;
+let nombre1=['Hugo',22];
+let nombre2=['Paco',12];
+let nombre3=['Luis',30];
+let basePersonas=[nombre1,nombre2,nombre3];
 function calcular() {
     
     sumar(numero);
@@ -14,7 +19,7 @@ function calcular1() {
 
 function calcular2() {
         
-    sumar2(numero);
+    sumar2(basePersonas);
 }
 
 
@@ -49,24 +54,32 @@ function sumar1(p1) {
 }
 
 
-function sumar2(p1) {  
+function sumar2(basePersonas) {  
 
-    let nombre1='Hugo';
-    let edad1=22;
-    let nombre2='Paco';
-    let edad2=12;
-    let nombre3='Luis';
+    
+    
+   
+    
 
-    for(let i = 0; i <= p1; i++){
+    for(let c = 0; c < basePersonas.length; c++){
         
+      
+        if(basePersonas[0][c]< 12){
+            console.log(basePersonas[0][c]);
+        }           
         
-        if(edad2 < 18)
-            {
+
+      }
+        
+
+
+        
+        /*if(edad2 < 18){
                 document.getElementById('name').innerHTML='Ciclo '+i+' Nombre = '+nombre2+' Edad = '+edad2;
                 break;
-            }else if(edad2 < 18){
+        }else if(edad2 < 18){
                 document.getElementById('name').innerHTML='Ciclo '+i+' Nombre = '+nombre2+' Edad = '+edad2;
-            }              
+        } */             
             
 
         
